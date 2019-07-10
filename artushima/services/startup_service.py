ffd08a36eadc4f70ac550e7abf50603de6f96cf4
Artq99 <amatracki@gmail.com>
2019-07-11 00:12:24 +0200

    add small changes to the startup_service

--- artushima/services/startup_service.py
+++ artushima/services/startup_service.py
@@ -2,12 +2,15 @@
 The service providing methods used on application startup.
 """
 
+from artushima.commons import logger
 from artushima.commons.exceptions import PersistenceError
 from artushima.commons.exceptions import BusinessError
+from artushima.persistence.decorators import transactional_service_method
 from artushima.internal_services import user_internal_service
 from artushima.services import service_utils
 
 
+@transactional_service_method
 def check_if_superuser_exists():
     """
     Check if the superuser exists.
@@ -19,9 +22,11 @@ def check_if_superuser_exists():
 
     try:
         superuser_exists = user_internal_service.check_if_user_exists("superuser")
-    except PersistenceError:
+    except PersistenceError as e:
+        logger.log_error(str(e))
         return service_utils.create_response_failure("Błąd odczytu danych.")
-    except BusinessError:
+    except BusinessError as e:
+        logger.log_error(str(e))
         return service_utils.create_response_failure("Błąd aplikacji.")
 
     return service_utils.create_response_success(superuser_exists=superuser_exists)
