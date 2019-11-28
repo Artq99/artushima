8aa7d08b048dc3fe8a5f0f54b2cba06a72030a83
Artq99 <amatracki@gmail.com>
2019-11-28 02:11:53 +0100

    add the data model module for the user component

--- artushima-db/migration/0001_V1.0_CREATE_TABLES.sql
+++ artushima-db/migration/0001_V1.0_CREATE_TABLES.sql
@@ -4,7 +4,8 @@ CREATE TABLE `user` (
     modified_on DATETIME NOT NULL,
     opt_lock INT UNSIGNED NOT NULL,
     password_hash VARCHAR(255),
-	user_name VARCHAR(255) NOT NULL
+	user_name VARCHAR(255) NOT NULL,
+    CONSTRAINT u_user_user_name UNIQUE (user_name)
 );
 
 CREATE TABLE user_history (
