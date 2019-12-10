00f50984fbbbfa86d39a957b66d7fbb346270b3e
Artq99 <amatracki@gmail.com>
2019-12-10 20:07:59 +0100

    implement auth_service

---
+++ artushima/auth/auth_service.py
@@ -0,0 +1,65 @@
+"""
+The module dealing with user authenication.
+"""
+
+from datetime import datetime, timedelta
+
+import jwt
+import werkzeug
+
+from artushima.core import properties
+from artushima.core.exceptions import BusinessError
+from artushima.core.properties import PROPERTY_TOKEN_EXPIRATION_TIME
+from artushima.user import user_roles_service, user_service
+
+
+def log_in(user_name, password):
+    """
+    Authenticate the user and return an authentication token.
+    """
+
+    user = user_service.get_user_by_user_name(user_name)
+
+    if user is None:
+        raise BusinessError(f"User {user_name} does not exist!")
+
+    if not werkzeug.check_password_hash(user["password_hash"], password):
+        raise BusinessError(f"Incorrect password!")
+
+    payload = _create_jwt_payload(user_name)
+    token = jwt.encode(payload, properties.get_app_secret_key(), algorithm="HS256")
+    user_roles = user_roles_service.get_user_roles(user_name)
+
+    return _create_log_in_response(user_name, user_roles, token)
+
+
+def _create_jwt_payload(user_name):
+    timestamp = datetime.utcnow()
+    exp_time = _get_and_validate_exp_time()
+
+    return {
+        "sub": user_name,
+        "iat": timestamp,
+        "exp": timestamp + timedelta(minutes=exp_time)
+    }
+
+
+def _get_and_validate_exp_time():
+    exp_time = properties.get_token_expiration_time()
+
+    if not exp_time:
+        raise BusinessError(f"Property {PROPERTY_TOKEN_EXPIRATION_TIME} missing!")
+
+    return int(exp_time)
+
+
+def _create_log_in_response(user_name, user_roles, token):
+    return {
+        "status": "success",
+        "message": "",
+        "currentUser": {
+            "userName": user_name,
+            "roles": user_roles,
+            "token": token
+        }
+    }
---
+++ tests/auth/auth_service_test.py
@@ -0,0 +1,76 @@
+"""
+The testing module for the auth service.
+"""
+
+from unittest import TestCase
+from unittest.mock import create_autospec
+
+import jwt
+import werkzeug
+
+from artushima.auth import auth_service
+from artushima.core import properties
+from artushima.core.exceptions import BusinessError
+from artushima.user import user_roles_service, user_service
+
+
+class LogInTest(TestCase):
+
+    def setUp(self):
+        self.user_service_mock = create_autospec(user_service)
+        self.user_roles_service_mock = create_autospec(user_roles_service)
+        self.properties_mock = create_autospec(properties)
+        self.werkzeug_mock = create_autospec(werkzeug)
+        self.jwt_mock = create_autospec(jwt)
+        auth_service.user_service = self.user_service_mock
+        auth_service.user_roles_service = self.user_roles_service_mock
+        auth_service.properties = self.properties_mock
+        auth_service.werkzeug = self.werkzeug_mock
+        auth_service.jwt = self.jwt_mock
+
+    def tearDown(self):
+        auth_service.user_service = user_service
+        auth_service.user_roles_service = user_roles_service
+        auth_service.werkzeug = werkzeug
+        auth_service.properties = properties
+        auth_service.jwt = self.jwt_mock
+
+    def test_should_authenticate_user(self):
+        # given
+        self.properties_mock.get_token_expiration_time.return_value = "60"
+        self.properties_mock.get_app_secret_key.return_value = "secret"
+
+        self.jwt_mock.encode.return_value = "test_token"
+
+        self.user_roles_service_mock.get_user_roles.return_value = {
+            "test_role_1",
+            "test_role_2"
+        }
+
+        # when
+        response = auth_service.log_in("test_user", "password")
+
+        # then
+        self.assertEqual("success", response["status"])
+        self.assertEqual("", response["message"])
+        self.assertEqual("test_user", response["currentUser"]["userName"])
+        self.assertEqual("test_token", response["currentUser"]["token"])
+        self.assertEqual(2, len(response["currentUser"]["roles"]))
+        self.assertIn("test_role_1", response["currentUser"]["roles"])
+        self.assertIn("test_role_2", response["currentUser"]["roles"])
+
+    def test_should_raise_error_when_user_does_not_exist(self):
+        # given
+        self.user_service_mock.get_user_by_user_name.return_value = None
+
+        # when then
+        with self.assertRaises(BusinessError):
+            auth_service.log_in("test_user", "password")
+
+    def test_should_raise_error_when_password_is_incorrect(self):
+        # given
+        self.werkzeug_mock.check_password_hash.return_value = False
+
+        # when then
+        with self.assertRaises(BusinessError):
+            auth_service.log_in("test_user", "password")
