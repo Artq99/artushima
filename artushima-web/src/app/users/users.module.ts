d451daa3fcc46c2cb715da5e0a5b32ade70799c2
Artq99 <amatracki@gmail.com>
2020-03-06 15:26:51 +0100

    add users module to the web-app

---
+++ artushima-web/src/app/users/users.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * The feature module containing functionalities related to user management.
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
