1d7cdaefaa09c40b4523cf44109275e10ec74ae1
Artq99 <amatracki@gmail.com>
2020-03-11 15:15:57 +0100

    fix existing tests for the user-creator

--- artushima-web/src/app/users/components/user-creator/user-creator.component.spec.ts
+++ artushima-web/src/app/users/components/user-creator/user-creator.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 import { UserCreatorComponent } from './user-creator.component';
 
@@ -9,7 +12,12 @@ describe('UserCreatorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UserCreatorComponent]
+      declarations: [UserCreatorComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ]
     }).compileComponents();
   }));
 
