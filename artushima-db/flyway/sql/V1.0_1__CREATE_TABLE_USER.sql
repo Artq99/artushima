cbbb94e86ffff4fb0ccd2340af7308dffb159aad
Artq99 <amatracki@gmail.com>
2020-03-13 23:33:04 +0100

    format the SQL scripts

--- artushima-db/flyway/sql/V1.0_1__CREATE_TABLE_USER.sql
+++ artushima-db/flyway/sql/V1.0_1__CREATE_TABLE_USER.sql
@@ -1,9 +1,10 @@
-CREATE TABLE `user` (
-	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_on DATETIME NOT NULL,
-    modified_on DATETIME NOT NULL,
-    opt_lock INT UNSIGNED NOT NULL,
-    password_hash VARCHAR(255),
-	user_name VARCHAR(255) NOT NULL,
-    CONSTRAINT u_user_user_name UNIQUE (user_name)
+CREATE TABLE user (
+	id              INT UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
+    created_on      DATETIME       NOT NULL,
+    modified_on     DATETIME       NOT NULL,
+    opt_lock        INT UNSIGNED   NOT NULL,
+    password_hash   VARCHAR(255),
+	user_name       VARCHAR(255)   NOT NULL,
+
+    UNIQUE (user_name)
 );
--- artushima-db/flyway/sql/V1.0_2__CREATE_TABLE_USER_HISTORY.sql
+++ artushima-db/flyway/sql/V1.0_2__CREATE_TABLE_USER_HISTORY.sql
@@ -1,12 +1,11 @@
 CREATE TABLE user_history (
-	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_on DATETIME NOT NULL,
-    modified_on DATETIME NOT NULL,
-    opt_lock INT UNSIGNED NOT NULL,
-    editor_name VARCHAR(255) NOT NULL,
-    message VARCHAR(255) NOT NULL,
-    user_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_user_history_user_id
-		FOREIGN KEY (user_id)
-        REFERENCES `user`(id)
+	  id            INT UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
+    created_on    DATETIME       NOT NULL,
+    modified_on   DATETIME       NOT NULL,
+    opt_lock INT  UNSIGNED       NOT NULL,
+    editor_name   VARCHAR(255)   NOT NULL,
+    message       VARCHAR(255)   NOT NULL,
+    user_id       INT UNSIGNED   NOT NULL,
+
+		FOREIGN KEY (user_id) REFERENCES user(id)
 );
