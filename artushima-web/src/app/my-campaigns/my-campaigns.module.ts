0a71c00b50eaf0d223eebc2383978c304007656c
Artq99 <amatracki@gmail.com>
2020-03-15 21:11:05 +0100

    add the module my-campaigns

---
+++ artushima-web/src/app/my-campaigns/my-campaigns.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * A feature module with functionalities for game masters.
+ *
+ * Enables the users with appropriate rights to view their campaigns, create
+ * new ones and manage existing.
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyCampaignsModule { }
