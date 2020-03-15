387fd24479c6d1e504b942c62150391e51facf27
Artq99 <amatracki@gmail.com>
2020-03-15 19:02:59 +0100

    add the role for viewing owned campaigns

--- artushima/user/roles.py
+++ artushima/user/roles.py
@@ -4,8 +4,10 @@ The module with constans and utils for user roles.
 
 ROLE_SHOW_USERS = "role_show_users"
 ROLE_CREATE_USER = "role_create_user"
+ROLE_SHOW_OWNED_CAMPAIGNS = "role_show_owned_campaigns"
 
 ALL_ROLES = [
     ROLE_SHOW_USERS,
-    ROLE_CREATE_USER
+    ROLE_CREATE_USER,
+    ROLE_SHOW_OWNED_CAMPAIGNS
 ]
