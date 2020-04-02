a80eadc21de9614eea2a3a2cfc62fdf0f726f3ac
Artq99 <amatracki@gmail.com>
2020-04-03 01:12:45 +0200

    implement the functionalities of the start-campaign component

--- artushima-web/src/app/my-campaigns/components/start-campaign/start-campaign.component.spec.ts
+++ artushima-web/src/app/my-campaigns/components/start-campaign/start-campaign.component.spec.ts
@@ -1,27 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
 
-import { StartCampaignComponent } from './start-campaign.component';
+import { MessagesService } from 'src/app/core/services/messages.service';
+import { MyCampaignsService } from '../../services/my-campaigns.service';
+
+import { StartCampaignComponent, DEFAULT_CAMPAIGN_START_DATE, MSG_CAMPAIGN_CREATED } from './start-campaign.component';
+import { RequestStatus } from 'src/app/core/model/request-status';
+import { MessageLevel } from 'src/app/core/model/message-level';
 
 describe('StartCampaignComponent', () => {
 
+  // The dependencies
+  let router: Router;
+  let messagesService: MessagesService;
+  let myCampaignsService: MyCampaignsService;
+
+  // The component under test
   let startCampaignComponent: StartCampaignComponent;
+
+  // The fixture
   let fixture: ComponentFixture<StartCampaignComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        StartCampaignComponent
-      ],
-      imports: [
-        FormsModule
-      ]
-    })
+    TestBed
+      .configureTestingModule({
+        declarations: [
+          StartCampaignComponent
+        ],
+        imports: [
+          RouterTestingModule,
+          HttpClientTestingModule,
+          FormsModule
+        ]
+      })
       .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StartCampaignComponent);
+    router = TestBed.get(Router);
+    messagesService = TestBed.get(MessagesService);
+    myCampaignsService = TestBed.get(MyCampaignsService);
+
     startCampaignComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -29,5 +53,53 @@ describe('StartCampaignComponent', () => {
   it('should be created', () => {
     // then
     expect(startCampaignComponent).toBeTruthy();
+  });
+
+  describe('startOnClick', () => {
+
+    // test data
+    let campaignName: string = 'Test campaign';
+
+    it('should navigate to the campaign list after a successful response', () => {
+      // given
+      let responseSubject: Subject<RequestStatus> = new BehaviorSubject<RequestStatus>(RequestStatus.SUCCESS);
+      let response$: Observable<RequestStatus> = responseSubject.asObservable();
+
+      spyOn(myCampaignsService, 'startCampaign')
+        .and.returnValue(response$);
+      spyOn(messagesService, 'showMessage');
+      spyOn(router, 'navigate');
+
+      startCampaignComponent.campaignName = campaignName;
+
+      // when
+      startCampaignComponent.startOnClick();
+
+      // then
+      expect(myCampaignsService.startCampaign).toHaveBeenCalledWith(campaignName, DEFAULT_CAMPAIGN_START_DATE);
+      expect(messagesService.showMessage).toHaveBeenCalledWith(MSG_CAMPAIGN_CREATED, MessageLevel.INFO);
+      expect(router.navigate).toHaveBeenCalledWith(['my_campaigns', 'list']);
+    });
+
+    it('should not navigate anywhere if the response was unsuccessful', () => {
+      // given
+      let responseSubject: Subject<RequestStatus> = new BehaviorSubject<RequestStatus>(RequestStatus.FAILURE);
+      let response$: Observable<RequestStatus> = responseSubject.asObservable();
+
+      spyOn(myCampaignsService, 'startCampaign')
+        .and.returnValue(response$);
+      spyOn(messagesService, 'showMessage');
+      spyOn(router, 'navigate');
+
+      startCampaignComponent.campaignName = campaignName;
+
+      // when
+      startCampaignComponent.startOnClick();
+
+      // then
+      expect(myCampaignsService.startCampaign).toHaveBeenCalledWith(campaignName, DEFAULT_CAMPAIGN_START_DATE);
+      expect(messagesService.showMessage).not.toHaveBeenCalled();
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
   });
 });
--- artushima-web/src/app/my-campaigns/components/start-campaign/start-campaign.component.ts
+++ artushima-web/src/app/my-campaigns/components/start-campaign/start-campaign.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MessagesService } from 'src/app/core/services/messages.service';
+import { MyCampaignsService } from '../../services/my-campaigns.service';
+
+import { RequestStatus } from 'src/app/core/model/request-status';
+import { MessageLevel } from 'src/app/core/model/message-level';
 
 /**
  * The default date for a campaign to begin with.
  * The date is the last mentioned date in the Neuroshima Handbook 1.5.
  */
-export const DEFAULT_CAMPAIGN_START_DATE = "2053-11-18";
+export const DEFAULT_CAMPAIGN_START_DATE = '2053-11-18';
+
+/**
+ * The message displayed as a campaign has been successfuly created.
+ */
+export const MSG_CAMPAIGN_CREATED = 'Kampania została utworzona.';
 
 /**
  * The component displaying a form for starting a new campaign with
@@ -15,7 +27,7 @@ export const DEFAULT_CAMPAIGN_START_DATE = "2053-11-18";
   templateUrl: './start-campaign.component.html',
   styleUrls: ['./start-campaign.component.scss']
 })
-export class StartCampaignComponent implements OnInit {
+export class StartCampaignComponent {
 
   @Input()
   public campaignName: string;
@@ -25,20 +37,29 @@ export class StartCampaignComponent implements OnInit {
 
   /**
    * @inheritdoc
+   *
+   * @param router the router
+   * @param messagesService the service for displaying messages
+   * @param myCampaignsService the service for managing the GM's campaigns
    */
-  public constructor() { }
-
-  /**
-   * @inheritdoc
-   */
-  public ngOnInit(): void { }
+  public constructor(
+    private router: Router,
+    private messagesService: MessagesService,
+    private myCampaignsService: MyCampaignsService
+  ) { }
 
   /**
    * The callback function for the button 'start'.
    */
   public startOnClick(): void {
-    console.log(this.campaignName);
-    console.log(this.beginDate);
+    this.myCampaignsService.startCampaign(this.campaignName, this.beginDate)
+      .subscribe(status => {
+        if (status === RequestStatus.SUCCESS) {
+          this.messagesService.showMessage(MSG_CAMPAIGN_CREATED, MessageLevel.INFO);
+          // TODO: After the successful response the user should be redirected to the campaign's page.
+          this.router.navigate(['my_campaigns', 'list']);
+        }
+      });
   }
 
 }
